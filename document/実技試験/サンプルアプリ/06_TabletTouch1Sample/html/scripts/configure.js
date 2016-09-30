358acaa8e5fb7b17374cebee13bc45b2c27a884b
pcpro1-0329 <pcpro.pepper.demo@gmail.com>
2016-10-01 03:39:21 +0900

    削除

--- document/実技試験/サンプルアプリ/06_TabletTouch1Sample/html/scripts/configure.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-	$("body").on("touchstart touchmove touchend",function(event){
-		event.preventDefault();
-		event.stopPropagation();
-	});
-
-	$.raiseALMemoryEvent("startYApp","OK");
-});
